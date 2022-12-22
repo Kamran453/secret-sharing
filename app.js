02cacc72d8645fa4041125199e94dc25a132ed95
Kamran453 <110389921+Kamran453@users.noreply.github.com>
2022-12-22 13:21:45 +0530

    upadte

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRETS,
-    callbackURL: process.env.CALLBACK_URL,
+    callbackURL: "https://secret-sharing.onrender.com/auth/google/secrets",
     // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
